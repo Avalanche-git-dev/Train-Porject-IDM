a3b47b678da4830d2e37912e6158ba407fd5e0da
Biagio Laino <biagio.laino@hotmail.com>
2024-10-21 21:58:01 +0200

    21:57

--- src/main/java/com/treno/application/controller/TrenoController.java
+++ src/main/java/com/treno/application/controller/TrenoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -153,8 +154,6 @@ public class TrenoController {
         trenoService.update(trenoDto);
         return "redirect:/treni/dettagli/" + trenoDto.getIdTreno();
     }
-
-  
 
     
     
